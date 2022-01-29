0aa927b076bc76a258351fc8e147bd7f42b29720
Ozan Tepe <ozantepe332@gmail.com>
2022-01-29 14:38:24 +0100

    Initial commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Ozan Tepe on 29.01.22.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
